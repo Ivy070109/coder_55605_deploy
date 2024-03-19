4ecb64a30772b6362fb933a871506902eefbd71d
Ivy070109 <Ivyaguilar07@gmail.com>
2024-03-19 20:21:42 -0300

    agregado los entornos

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import sessionsRouter from './routes/sessions.router.js';
 
 const app = express();
 const PORT = process.env.PORT||8080;
-const connection = mongoose.connect(`URL DE MONGO`)
+const connection = mongoose.connect(`mongodb+srv://ivyaguilar07:bjLpjWzJQGdcrVRL@cluster0.2olteyc.mongodb.net/coder55605`)
 
 app.use(express.json());
 app.use(cookieParser());
